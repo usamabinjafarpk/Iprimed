0f013f4bfc3e96b05d404bab77c6fa7b64ee369a
USAMA BIN JAFAR PK <67426021+usamabinjafarpk@users.noreply.github.com>
2024-01-04 17:38:45 +0530

    Add files via upload

---
+++ Asp.net core/Day9/HandsOnLinq/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnLinq
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            string[] flowers = { "rose", "tulip", "lotus" };
+            string[] fruits = { "apple", "orange", "mango" };
+
+            var res=from item in flowers
+                    where item.Length>4
+                    select item.ToUpper();
+            foreach(var r in res)
+            {
+                Console.WriteLine(r);
+            }
+            var res1=from item in fruits
+                     orderby item
+                     select item;
+            var res2=from item in fruits
+                     select item.Length;
+            Console.WriteLine();
+            foreach(var r in res1)
+            {
+                Console.WriteLine(r.ToUpper());
+            }
+        }
+    }
+}
---
+++ Asp.net core/Day9/HandsOnLinq/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace HandsOnLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = {  5, 6, 7, 8, 1, 2, 3, 4, 9, 10, 11, 12, 13, 14, };
+            var result=from k in n
+                       where k > 8
+                       select k;
+            var res = from k in n
+                      select k * k;
+            res=from k in n
+                     where k*k > 100
+                     select k*k;
+            res = from k in n
+                  let i = k * k
+                  select i;
+            res = from k in n
+                  let i=k*k
+                  where i>100
+                  select i;
+            res = from k in n
+                  orderby k
+                  select k;
+            res = from k in n
+                  orderby k descending
+                  select k;
+            res = from k in n
+                  let i = k * k
+                  where i > 100 && i < 150
+                  select i;
+         
+            
+
+            foreach (var v in result)
+            {
+                Console.WriteLine(v);
+            }
+            Console.WriteLine();
+            foreach (var v in res)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
