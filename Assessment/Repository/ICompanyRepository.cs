3d3bd03a65231eb2774505351cbe59140b54b94b
USAMA BIN JAFAR PK <67426021+usamabinjafarpk@users.noreply.github.com>
2024-01-08 17:39:20 +0530

    Add files via upload

---
+++ Assessment/Repository/ICompanyRepository.cs
@@ -0,0 +1,15 @@
+﻿using Assessment.Models;
+
+namespace Assessment.Repository
+{
+    public interface ICompanyRepository
+    {
+       
+        void InputCompany(Company company);
+        List<Company> GetAllCompanies();
+        List<Company> GetCompaniesByCity(string city);
+        Company GetCompanyById(int id);
+        void DeleteCompany(int CompanyId);
+
+    }
+}
