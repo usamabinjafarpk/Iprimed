13aef9010f202b8529e302e3ee11368c4aab58f8
USAMA BIN JAFAR PK <67426021+usamabinjafarpk@users.noreply.github.com>
2024-01-24 18:02:00 +0530

    react

--- Web Development/React/HandsOn/react_app/src/index.js
+++ Web Development/React/HandsOn/react_app/src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const myElement=(
+  <div>
+    <table>
+      <tr>
+        <th>Name</th>
+      </tr>
+      <tr>
+        <th>Usama</th>
+      </tr>
+    </table>
+  </div>
+)
 root.render(
   <React.StrictMode>
     <App />
+    {/* {myElement} */}
   </React.StrictMode>
 );
+// ReactDOM.render(<p>hello</p>,document.getElementById("root"))
+
+// ReactDOM.render(myElement,document.getElementById("root"))
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
