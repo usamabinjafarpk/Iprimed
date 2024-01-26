41d534905103bd33167cf30dde7aae698757529c
USAMA BIN JAFAR PK <67426021+usamabinjafarpk@users.noreply.github.com>
2024-01-26 22:48:17 +0530

    a

--- Entity Framework/Assignment/EF_Assignment/obj/Debug/net8.0/EF_Assignment.AssemblyInfo.cs
+++ Entity Framework/Assignment/EF_Assignment/obj/Debug/net8.0/EF_Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EF_Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a574671267fc58e809d26c961a8b1964b8d0214")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EF_Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EF_Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
